dc973b1661334ada58126102ebe68f921eb4de71
Aleksandr Kelbas <alex@motionly.co.uk>
2023-08-10 08:31:01 +0100

    Initial Commit

---
+++ unsplash-blog/unsplash_blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  unsplash_blogApp.swift
+//  unsplash-blog
+//
+//  Created by Aleksandr Kelbas on 10/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct unsplash_blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
